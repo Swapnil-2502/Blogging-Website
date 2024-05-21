1e2015d03a91a15d489e4875218a2d4a5cbd01f5
Swapnil-2502 <hajareswapnil.2502@gmail.com>
2024-05-21 23:22:53 +0530

    Routing in done

---
+++ frontend/src/pages/Signup.tsx
@@ -0,0 +1,18 @@
+import { Quote } from "../components/Quote"
+
+export const Signup = () =>{
+    return (
+        <div>
+            <div className="grid md:grid-cols-2">
+                <div>
+
+                </div>
+                <div className="hidden md:block">
+                     <Quote />
+                </div>
+                
+            </div>
+        </div>
+       
+    )
+}
