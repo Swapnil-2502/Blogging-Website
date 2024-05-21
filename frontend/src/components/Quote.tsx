de95934570b4a26cbcc6608cf19fcd6d9a112303
Swapnil-2502 <hajareswapnil.2502@gmail.com>
2024-05-21 23:22:21 +0530

    created components and added quote

---
+++ frontend/src/components/Quote.tsx
@@ -0,0 +1,18 @@
+
+export const Quote = () => {
+    return (
+
+        <div className="bg-gray-400 h-screen flex flex-col justify-center" >
+            <div className="flex justify-center">
+                <div className="max-w-md text-base md:text-2xl lg:text-3xl font-bold">
+                "The purpose of life is to live it, to taste experience to the utmost, to reach out eagerly and without fear for newer and richer experience."
+                </div>
+            </div>
+            <div className="max-w-md text-center text-xl font-semibold pt-5 underline">
+                    Eleanor Roosevelt
+                </div>
+           
+        </div>
+
+    )
+}
